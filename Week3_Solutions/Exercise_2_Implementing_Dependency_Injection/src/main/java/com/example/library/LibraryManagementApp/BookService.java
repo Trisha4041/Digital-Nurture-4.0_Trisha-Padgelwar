e135369dd23382a4a386ef6c7a3fc4b92dc5ecc1
Trisha4041 <146335284+Trisha4041@users.noreply.github.com>
2025-07-05 21:25:41 +0530

    Add Exercise 2: Implementing Dependency Injection

---
+++ Week3_Solutions/Exercise_2_Implementing_Dependency_Injection/src/main/java/com/example/library/LibraryManagementApp/BookService.java
@@ -0,0 +1,15 @@
+package com.example.library.LibraryManagementApp;
+
+public class BookService {
+    private BookRepository bookRepository;
+
+    // Setter for DI
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void addBook(String bookName) {
+        System.out.println("Adding book: " + bookName);
+        bookRepository.saveBook(bookName);
+    }
+}
